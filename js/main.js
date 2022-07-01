276f153fdaac3e8fb394e76cdcc48886ce85ee95
Majed <30793438+majedkarimi@users.noreply.github.com>
2022-07-01 15:24:19 +0430

    majed

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const boxProfile = document.querySelector(".show-sub");
 btnOpenProfile.addEventListener("click", function (e) {
   e.preventDefault();
   console.log("acition");
+  console.log("thi is makedd ");
   if (boxProfile.classList.contains("show")) {
     boxProfile.classList.remove("show");
   } else {
